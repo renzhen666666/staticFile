70b9cc2ea417d0acf4a3c9e284e9f60ee3cc9d1a
renzhen666666 <151754655+renzhen666666@users.noreply.github.com>
2026-02-09 18:23:44 +0800

    bug

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://static-b.rz7.top',
         changeOrigin: true,
       },
     },
